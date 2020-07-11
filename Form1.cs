1b90580d23af4d470ee18e94f3f3cd5fd580c929
Ronielle <adenilson.santos95@academico.ifs.edu.br>
2020-07-11 15:50:55 -0300

    Algoritmo simples que mostra o uso do polimorfismo para desenhar formas geometricas.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormasGeomatricas_FocoNoUsoDoPolimorfismo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_executar_Click(object sender, EventArgs e)
+        {
+            Retangulo retangulo = new Retangulo();
+            retangulo.Altura = 50;
+            retangulo.Largura = 100;
+            retangulo.Desenhar();
+            retangulo.Apresentar();
+            retangulo.desenharOutro();
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.Altura = 150;
+            triangulo.Largura = 200;
+            triangulo.Desenhar();
+            triangulo.Apresentar();
+            triangulo.desenharOutro();
+
+            Circunferencia circunferencia = new Circunferencia();
+            circunferencia.Altura = 250;
+            circunferencia.Largura = 300;
+            circunferencia.Desenhar();
+            circunferencia.Apresentar();
+            circunferencia.desenharOutro();
+        }
+    }
+}
+
---
+++ FormasGeometricas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormasGeomatricas_FocoNoUsoDoPolimorfismo
+{
+     abstract class FormasGeometricas
+    {
+        private int _largura;
+        private int _altura;
+
+        public int Largura
+        {
+            get { return _largura; }
+            set { _largura = value; }
+        }
+
+        public int Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine("Este método pertence a classe base.");
+        }
+
+        public abstract void Desenhar();
+
+        public virtual void desenharOutro()
+        {
+            Console.WriteLine("Método virtual da clase base.");
+        }
+ 
+    }
+
+    class Retangulo : FormasGeometricas
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhar um retangulo com dimensões : " + Largura + " x " + Altura);
+        }
+
+        public override void desenharOutro()
+        {
+            Console.WriteLine("Este é um método virtual substituido na classe derivada.");
+        }
+    }
+
+    class Triangulo : FormasGeometricas
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhar um triangulo com dimensões : " + Largura + " x " + Altura);
+        }
+    }
+
+    class Circunferencia : FormasGeometricas
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhar uma circunferência com dimensões : " + Largura + " x " + Altura);
+        }
+    }
+}
